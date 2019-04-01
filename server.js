b92d0a0d5b37b4852181ac887fa958120e26015e
ramalamadingdong <ramimouro@gmail.com>
2019-04-01 16:21:51 -0600

    initial upload

---
+++ server.js
@@ -0,0 +1,81 @@
+/***********************
+  Load Components!
+  ***********************/
+var express = require('express'); //Ensure our express framework has been added
+var app = express();
+var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
+app.use(bodyParser.json());              // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+//Create Database Connection
+var pgp = require('pg-promise')();
+
+/**********************
+  Database Connection information
+  host: This defines the ip address of the server hosting our database.  We'll be using localhost and run our database on our local machine (i.e. can't be access via the Internet)
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  From our previous lab, we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database.  You'll need to set a password USING THE PSQL TERMINAL THIS IS NOT A PASSWORD FOR POSTGRES USER ACCOUNT IN LINUX!
+  **********************/
+  const dbConfig = {
+   host: 'localhost',
+   port: 5432,
+   database: 'project',
+   user: 'postgres',
+   password: 'user'
+ };
+
+ var db = pgp(dbConfig);
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
+
+// registration page 
+app.get('/', function(req, res) {
+  res.render('pages/login',{
+    my_title:"Log in"
+  });
+});
+
+
+// registration page 
+app.get('/register', function(req, res) {
+	res.render('pages/register',{
+		my_title:"Registration Page"
+	});
+});
+
+app.post('/register/submit', function(req, res) {
+  var fname = req.body.fname;
+  var lname = req.body.lname;
+  var username = req.body.uname;
+  var email = req.body.email;
+  var password = req.body.psw;
+
+  var insert_statement = "INSERT INTO acounts(firstname, lastname, username, email, password) VALUES('" + fname + "','" + 
+  lname + "','" + username + "','" + email + "','" + password +"') ON CONFLICT DO NOTHING;";
+
+  db.task('get-everything', task => {
+    return task.batch([
+      task.any(insert_statement),
+      ]);
+  })
+  .then(info => {
+    res.render('pages/login',{
+      my_title: "Log in",
+    })
+  })
+  .catch(error => {
+        // display error message in case an error
+        request.flash('error', err);
+        response.render('pages/register', {
+          title: 'Registration Failed',
+
+        })
+      }); 
+});
+
+app.listen(3000);
+console.log('3000 is the magic port');
