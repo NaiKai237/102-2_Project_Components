16d1d9bb5d682d1844198c1a60b74724a6b20389
yairbennaim <46978032+yairbennaim@users.noreply.github.com>
2019-04-02 16:35:21 -0600

    Add files via upload

---
+++ LoChat.js
@@ -0,0 +1,41 @@
+//registration page
+//doc.getelemets for all the userinput
+
+var firstName = document.getElementsByName("fname");
+
+console.log("Hello");
+
+function password8char()
+{
+//make sure passwords is atleast 8 chars, return bool
+}
+
+function confirmPasswords()
+{
+//make sure passwword matches confirm
+}
+
+function namesExist()
+{
+	console.log("In func");
+	if (firstName == "Bob")
+	{
+		alert("Nice");
+	}
+	//make sure there is some input in first name and last name
+}
+
+function uniqueUser()
+{
+//database check
+}
+
+
+function enableButton()
+{
+}
+//if all of the above are true, enable the button
+
+document.getElementsByTagName("sumbit").onclick = namesExist();
+
+
