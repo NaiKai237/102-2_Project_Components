05b10675400c9d049c37f98fcd5f53872b9bf695
yairbennaim <46978032+yairbennaim@users.noreply.github.com>
2019-04-15 17:17:44 -0600

    Update LoChat.js

--- LoChat.js
+++ LoChat.js
@@ -1,41 +1,73 @@
 //registration page
 //doc.getelemets for all the userinput
 
-var firstName = document.getElementsByName("fname");
+var firstName = document.getElementsByName("fname").value;
+var lastName = document.getElementsByName("lname").value;
+var userName = document.getElementsByName("uname").value;
+var email = document.getElementsByName("email").value;
+var password = document.getElementsByName('psw').value;
+var confirmPassword = document.getElementsByName("psw-repeat").value;
 
-console.log("Hello");
+document.getElementsByName("RegSub").disabled =true;
+
 
 function password8char()
 {
+	
+	if (password.length >= 8)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
 //make sure passwords is atleast 8 chars, return bool
 }
 
 function confirmPasswords()
 {
-//make sure passwword matches confirm
+	if (password == confirmPassword)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
 }
 
 function namesExist()
 {
-	console.log("In func");
-	if (firstName == "Bob")
+	if (firstName.length > 0 && lastName.length > 0)
 	{
-		alert("Nice");
+		return true;
 	}
-	//make sure there is some input in first name and last name
+
+	else
+	{
+		return false;
+	}
 }
 
-function uniqueUser()
-{
-//database check
-}
+//function uniqueUser()
+//{
+////database check
+//}
 
 
 function enableButton()
 {
+	console.log(email);
+
+
+	if (password8char() == true && confirmPasswords() == true && namesExist() == true)
+	{
+		document.getElementsByName("RegSub").disabled =false;
+		console.log("Hello");
+	}
+
 }
 //if all of the above are true, enable the button
-
-document.getElementsByTagName("sumbit").onclick = namesExist();
-
-
