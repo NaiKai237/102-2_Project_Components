84661ac83f4e2f216ceb23e81b9e22aca66f1ee3
JosiahGraham <josiah.graham@colorado.edu>
2019-04-16 11:07:57 -0600

    Cleaned up code in LoChat.js, no changes to functionality.

--- LoChat.js
+++ LoChat.js
@@ -8,48 +8,20 @@ var email = document.getElementsByName("email").value;
 var password = document.getElementsByName('psw').value;
 var confirmPassword = document.getElementsByName("psw-repeat").value;
 
-document.getElementsByName("RegSub").disabled =true;
+document.getElementsByName("RegSub").disabled = true;
 
 
-function password8char()
-{
-	
-	if (password.length >= 8)
-	{
-		return true;
-	}
-
-	else
-	{
-		return false;
-	}
 //make sure passwords is atleast 8 chars, return bool
+function password8char() {
+	return (password.length >= 8)
 }
 
-function confirmPasswords()
-{
-	if (password == confirmPassword)
-	{
-		return true;
-	}
-
-	else
-	{
-		return false;
-	}
+function confirmPasswords() {
+	return (password == confirmPassword)
 }
 
-function namesExist()
-{
-	if (firstName.length > 0 && lastName.length > 0)
-	{
-		return true;
-	}
-
-	else
-	{
-		return false;
-	}
+function namesExist() {
+	return (firstName.length > 0 && lastName.length > 0)
 }
 
 //function uniqueUser()
@@ -63,9 +35,9 @@ function enableButton()
 	console.log(email);
 
 
-	if (password8char() == true && confirmPasswords() == true && namesExist() == true)
+	if (password8char() && confirmPasswords() && namesExist())
 	{
-		document.getElementsByName("RegSub").disabled =false;
+		document.getElementsByName("RegSub").disabled = false;
 		console.log("Hello");
 	}
 
